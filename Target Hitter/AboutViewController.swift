956608d49e8b22944148f652c4ab5ebdb75713de
Liam DeBeasi <liamdebeasi@icloud.com>
2016-11-07 00:30:45 -0500

    Add final source code

---
+++ Target Hitter/AboutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.swift
+//  Target Hitter
+//
+//  Created by Aleksander Skjoelsvik on 11/3/16.
+//  Copyright © 2016 Aleksander Skjoelsvik. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+	@IBAction func close() {
+		dismiss(animated: true, completion: nil)
+	}
+}
